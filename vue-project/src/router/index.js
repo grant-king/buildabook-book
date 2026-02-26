cc7cae3b20bb1ec5d532534a26112b2a5723cd06
Grant King <grantryan.king@gmail.com>
2026-02-26 15:06:05 -0800

    Add intro and chapter000 routes
    Adds a lazy-loaded '/intro' route for IntroductionView and a '/chapter000' route for Chapter000View using dynamic imports (route-level code-splitting).
    
    Inserts marker comments ([[CHAPTER_PATHS]] and [[END_CHAPTER_PATHS]]) to delineate auto-generated chapter routes; the existing chpater routes will be overwritten by the generated route list.

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -18,10 +18,25 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
     },
     {
+      path: '/intro',
+      name: 'intro',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/IntroductionView.vue'),
+    },
+    //[[CHAPTER_PATHS]]
+    {
+      path: '/chapter000',
+      name: 'chapter000',
+      component: () => import('../views/Chapter000View.vue'),
+    },
+    {
       path: '/chapter001',
       name: 'chapter001',
       component: () => import('../views/Chapter001View.vue'),
     },
+    //[[END_CHAPTER_PATHS]]
   ],
 })
 
