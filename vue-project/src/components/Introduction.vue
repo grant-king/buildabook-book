bc9de883c0e77bfa77ad51b5b39a3e08cde8a139
Grant King <grantryan.king@gmail.com>
2026-03-30 06:10:45 -0700

    Update Introduction.vue
    Add a small padding for the content inside the dedication block in order to improve the visual presentation.

--- vue-project/src/components/Introduction.vue
+++ vue-project/src/components/Introduction.vue
@@ -6,7 +6,7 @@
     <div class="max-w-2xl mx-auto w-full">
         <div class="border-4 border-black/40 rounded">
             <div
-                class="grid grid-cols-1 gap-2 text-xl font-semibold text-center text-white font-serif bg-black/40 rounded py-16 border-16 border-double border-white/80">
+                class="p-4 grid grid-cols-1 gap-2 text-xl font-semibold text-center text-white font-serif bg-black/40 rounded py-16 border-16 border-double border-white/80">
                 [[DEDICATION_BLOCKS]]
             </div>
         </div>
